4a730a87eeb0af5c22d2750d29056ba004be1366
Young-TW <young20050727@gmail.com>
2021-05-17 20:12:57 +0800

    add some py

---
+++ a002.py
@@ -0,0 +1,6 @@
+a=input('input n1=')
+a=int(a)
+b=input('input n2=')
+b=int(b)
+c = a + b
+print(c)
--- a005.cpp
+++ a005.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 int main() {
-  int n1,n2,n3,n4,n5,t;
+  int n[5],t;
   cin >> t;
   while (t>0){
-    cin >> n1 >> n2 >> n3 >> n4;
-    if (n4-n3!=n3-n2){
-      n5 = n4 * (n4 / n3);
-      cout << n1 << " " << n2 << " " << n3 << " " << n4 << " " << n5 << "\n";
+    cin >> n[1] >> n[2] >> n[3] >> n[4];
+    if (n[4]-n[3]!=n[3]-n[2]){
+      n[5] = n[4] * (n[4] / n[3]);
+      cout << n[1] << " " << n[2] << " " << n[3] << " " << n[4] << " " << n[5] << "\n";
     }
-    else if (n4-n3==n3-n2){
-      n5 = n4 + (n4-n3);
-      cout << n1 << " " << n2 << " " << n3 << " " << n4 << " " << n5 << "\n";
+    else if (n[4]-n[3]==n[3]-n[2]){
+      n[5] = n[4] + (n[4]-n[3]);
+      cout << n[1] << " " << n[2] << " " << n[3] << " " << n[4] << " " << n[5] << "\n";
     }
-    n1=n2=n3=n4=n5=0;
+    n[1]=n[2]=n[3]=n[4]=n[5]=0;
     t=t-1;
   }
   return 0;
