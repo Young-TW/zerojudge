662388817ba9270593b93a8f5490282050c6e94d
Young-TW <young20050727@gmail.com>
2021-09-05 11:45:29 +0800

    d058 ac

---
+++ d058.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    if(n>0){
+    	cout << "1";
+	}else if(n==0){
+		cout << "0";
+	}else{
+		cout << "-1";
+	}
+    return 0;
+}
