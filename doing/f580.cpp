61e46260c29f42dbad589e0ddcc5b946ab6d08aa
Young-TW <young20050727@gmail.com>
2022-10-04 11:38:09 +0800

    d460

---
+++ doing/f580.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n,m,a,b;
+    cin >> n >> m;
+    vector<int> ans(n);
+    for(int i=0;i<m;i++){
+        cin >> a >> b;
+        switch(b){
+            case -1:
+                break;
+            case -2:
+                break;
+        }
+    }
+    return 0;
+}
