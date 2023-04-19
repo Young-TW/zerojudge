50e7a5a23be613832c800274bc97ac0e3cd832ba
young <young20050727@gmail.com>
2023-04-19 21:59:58 +0800

    e051

---
+++ src/ac/e/e051.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string str;
+  cin >> str;
+  cout << str.front();
+  for (int i = 0; i < str.length() - 2; i++) {
+    cout << '_';
+  }
+  cout << str.back();
+  return 0;
+}
