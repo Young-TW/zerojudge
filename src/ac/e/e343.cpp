e59a828d774ae8205d8ff156f4df5ea36af4cd0f
Young <young20050727@gmail.com>
2026-07-08 15:22:59 +0800

    fix(e343): 修正身高單位（題目為公尺而非公分）

--- src/ac/e/e343.cpp
+++ src/ac/e/e343.cpp
@@ -4,9 +4,7 @@
 int main() {
     double h, w, ans;
     std::cin >> w >> h;
-    h = h / 100;
-    h = h * h;
-    ans = w / h;
+    ans = w / (h * h);
     std::cout << std::fixed << std::setprecision(1) << ans;
     return 0;
 }
