9d80b4a8ee486f59f1f7c4792f4235b777d59e6c
Young <young20050727@gmail.com>
2026-07-08 15:41:17 +0800

    revert: 把 subagent 改的 src/ac/ 還原成 OJ AC 版
    src/ac/ 應該只放 OJ 上 AC 過的正解。這次還原：
    - a005, a009, e343：subagent 修改的 3 個 OJ AC 檔，還原成原版
    - a021, a095, e340, e378, h900：subagent 新增到 src/ac/ 的 5 個檔
      （這些沒 OJ 確認，搬到 src/doing/）

--- src/ac/a/a009.cpp
+++ src/ac/a/a009.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-#include <string>
 
 int main() {
     std::string a;
     while (std::getline(std::cin, a)) {
-        if (!a.empty() && a.back() == '\r') a.pop_back();
-        for (char c : a) {
-            std::cout << char(c - 7);
+        for (int x = 0; x < a.length(); x++) {
+            std::cout << char(a[x] - 7);
         }
-        std::cout << "\r\n";
+
+        std::cout << std::endl;
     }
+
     return 0;
 }
-
--- src/ac/e/e343.cpp
+++ src/ac/e/e343.cpp
@@ -4,7 +4,9 @@
 int main() {
     double h, w, ans;
     std::cin >> w >> h;
-    ans = w / (h * h);
+    h = h / 100;
+    h = h * h;
+    ans = w / h;
     std::cout << std::fixed << std::setprecision(1) << ans;
     return 0;
 }
