012214064fd5ce8cfc5be18ac78646a1a393da29
Young <young20050727@gmail.com>
2026-07-13 20:19:42 +0800

    promote(orch cycle 551): 0 AC -> src/ac, 1 非AC -> doing

---
+++ src/doing/q/q878.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    std::cout << "Anon\n";
+    std::cout << "dict\n";
+    std::cout << "{\"band\":\"AnonTokyo\",\"singer\":\"Tomorin\",\"Fans\":122}\n";
+    return 0;
+}
